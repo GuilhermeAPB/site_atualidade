0b3ce9e08deb8221621b8d54c22c24ac989e6e73
Bezerra <guilherme.a.bezerra@accenture.com>
2024-12-18 11:13:31 -0300

    Retirando o _secret no nome das variaveis

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from telegram.ext import Application, CommandHandler
 from bot.news_fetcher import buscar_noticias
 from bot.rewriter import reescrever_noticia
 from bot.telegram_bot import enviar_noticias, reescrever
-from config import TELEGRAM_API_KEY_SECRET, OPENAI_API_KEY_SECRET
+from config import TELEGRAM_API_KEY, OPENAI_API_KEY
 import openai
 
 # Aplica o ajuste para o loop de eventos no Google Colab
 nest_asyncio.apply()
 
-openai.api_key = OPENAI_API_KEY_SECRET
+openai.api_key = OPENAI_API_KEY
 
 # Função principal para rodar o bot
 async def main():
-    application = Application.builder().token(TELEGRAM_API_KEY_SECRET).build()
+    application = Application.builder().token(TELEGRAM_API_KEY).build()
 
     # Handlers de comandos
     application.add_handler(CommandHandler("start", buscar_noticias))  # Iniciar ao enviar o comando /start
--- seu_script.py
+++ seu_script.py
@@ -1,8 +1,8 @@
 import os
 
 # Acessando as chaves de API que foram passadas pelo GitHub Actions
-telegram_api_key = os.getenv('TELEGRAM_API_KEY')
-news_api_key = os.getenv('OPENAI_API_KEY')
+TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-print("Usando a chave do TELEGRAM:", telegram_api_key)
-print("Usando a chave da OPENAI API:", news_api_key)
+print("Usando a chave do TELEGRAM:", TELEGRAM_API_KEY)
+print("Usando a chave da OPENAI API:", OPENAI_API_KEY)
